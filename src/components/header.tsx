d38ea096314b665a00962f44fa4bfa53119e8df2
Iceman8911 <wuchiJSS3@gmail.com>
2025-07-26 22:49:13 +0100

    wip(header): made the clickable links / buttons look better

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,16 +7,24 @@ export default function Header() {
 		<h1 class="h-20 mb-4 text-3xl font-bold flex justify-center items-center gap-2">
 			<img class="h-10 w-auto" src="/pwa-64x64.png" alt="Dictionary Logo" />
 
-			<A href="/" end>
+			<A href="/" class="hover:link mr-8" end>
 				Lexi Cache
 			</A>
 
 			<A href="/settings" end>
-				<SettingsIcon class="h-8 w-auto ml-8" />
+				<div class="tooltip tooltip-bottom" data-tip="Settings">
+					<button class="btn btn-circle p-1.5">
+						<SettingsIcon class="h-8 w-auto" />
+					</button>
+				</div>
 			</A>
 
 			<A href="/about" end>
-				<InfoIcon class="h-8 w-auto md:ml-4" />
+				<div class="tooltip tooltip-bottom" data-tip="About">
+					<button class="btn btn-circle p-1.5">
+						<InfoIcon class="h-8 w-auto" />
+					</button>
+				</div>
 			</A>
 		</h1>
 	);
